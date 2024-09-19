478276667c8f577076f9ebf5ce1c1c5b457821e3
AymaneOnline <138299321+AymaneOnline@users.noreply.github.com>
2024-09-20 00:24:48 +0100

    My Solution

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Hash;
+
 
 class ProfileController extends Controller
 {
@@ -15,6 +17,21 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        
+        // Get the currently authenticated user
+        $user = auth()->user();
+
+        // Update the name and email
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+
+        // Check if a password is set, and update it if so
+        if ($request->filled('password')) {
+            $user->password = Hash::make($request->input('password'));
+        }
+
+        // Save the changes to the user
+        $user->save();
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
